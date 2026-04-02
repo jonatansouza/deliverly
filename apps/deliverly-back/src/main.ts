021f472cfd05962bfba6f9754532c95f9cb2bca2
Jonatan Gall <jonatangd.souza@gmail.com>
2026-04-02 19:49:47 -0300

    initialize kafka

--- apps/deliverly-back/src/main.ts
+++ apps/deliverly-back/src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { kafkaConsumerConfig } from './infra/message-broker/kafka.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
 
-  //const configService = app.get(ConfigService);
-  //kafkaConsumerConfig(app, configService);
+  const configService = app.get(ConfigService);
+  kafkaConsumerConfig(app, configService);
 
-  //await app.startAllMicroservices();
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
